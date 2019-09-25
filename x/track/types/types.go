a1ad44338c29b5b5ec740a24e8a904bbf493bb80
Angelo <angelo@rcstart.it>
2019-09-25 15:40:51 +0200

    Renamed Track types from Song to Track

--- x/track/types/types.go
+++ x/track/types/types.go
@@ -8,9 +8,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// Song is a struct that contains all the metadata of a title
-type Song struct {
-	SongID                  uint64         `json:"song_id"`
+// Track is a struct that contains all the metadata of a title
+type Track struct {
+	TrackID                 uint64         `json:"track_id"`
 	Owner                   sdk.AccAddress `json:"owner"`
 	Title                   string         `json:"title"`
 	Content                 string         `json:"content"`
@@ -19,25 +19,25 @@ type Song struct {
 	CreateTime              time.Time      `json:"create_time"`
 }
 
-func (s Song) String() string {
-	return fmt.Sprintf(`Song %d:
+func (s Track) String() string {
+	return fmt.Sprintf(`Track %d:
 		  Owner:					%s
 		  Title:					%s
 		  Content:					%s
 		  TotalReward:				%s
-		  RedistributionSplitRate:	%s
-		  Create Time:		%s`, s.SongID, s.Owner, s.Title, s.Content, s.TotalReward, s.RedistributionSplitRate, s.CreateTime)
+		  RedistributionSplitRate:	%d
+		  Create Time:		%s`, s.TrackID, s.Owner, s.Title, s.Content, s.TotalReward, s.RedistributionSplitRate, s.CreateTime)
 }
 
-// Songs is an array of track
+// Tracks is an array of track
 // To FIX with new fields
-type Songs []*Song
+type Tracks []*Track
 
-func (songs Songs) String() string {
+func (tracks Tracks) String() string {
 	out := fmt.Sprintf("%10s - (%15s) - (%40s) - [%10s] - Create Time\n", "ID", "Title", "Owner", "CreateTime")
-	for _, song := range songs {
+	for _, track := range tracks {
 		out += fmt.Sprintf("%10d - (%15s) - (%40s) - [%10s]\n",
-			song.SongID, song.Title, song.Owner, song.CreateTime)
+			track.TrackID, track.Title, track.Owner, track.CreateTime)
 	}
 
 	return strings.TrimSpace(out)
