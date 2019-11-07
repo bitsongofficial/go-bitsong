dab9951ef8d099926f8808f348db336445d9fcbc
Angelo <angelo@rcstart.it>
2019-11-07 02:27:53 +0100

    dev_setup script

---
+++ scripts/dev_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+GO_VERSION=1.12.13
+
+echo "Update Ubuntu..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installs packages necessary to run go..."
+sudo apt install build-essential libleveldb1v5 git unzip -y
+
+echo "Install go${GO_VERSION}..."
+sudo wget -c https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+
+echo "Updates environmental variables to include go..."
+cat <<EOF >> $HOME/.profile  
+export GOPATH=\$HOME/go  
+export GO111MODULE=on  
+export PATH=\$PATH:/usr/local/go/bin:\$HOME/go/bin  
+EOF
+source $HOME/.profile
+
+echo "Finish!"
