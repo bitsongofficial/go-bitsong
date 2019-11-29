3b9d92d3931fdb655bb82f0b0039251112fae6b3
Angelo <angelo@rcstart.it>
2019-11-29 15:54:53 +0100

    interim moderator

---
+++ types/util/moderator.go
@@ -0,0 +1,7 @@
+package util
+
+const (
+	// Interim moderator that can edit status on modules
+	// This is only for testnet use and will be excluded then
+	ModeratorBech32AccAddr = "bitsong1wh2x5lp08dmgelmyrwmey6pmznm9wk3pegs89a"
+)
