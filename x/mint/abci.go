364e9d178fafaac2a9e47b69616715bb8e88af40
Angelo <angelo@rcstart.it>
2019-12-06 15:23:13 +0100

    Separate mint module from sdk

--- x/mint/abci.go
+++ x/mint/abci.go
@@ -1,1 +1,10 @@
 package mint
+
+import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func BeginBlocker(ctx sdk.Context, k Keeper) {
+	fmt.Printf("OKK22222222222222222222")
+}
--- x/mint/cosmos_alias.go
+++ x/mint/cosmos_alias.go
@@ -1,1 +1,23 @@
 package mint
+
+import (
+	mintCosmos "github.com/cosmos/cosmos-sdk/x/mint"
+)
+
+const (
+	ModuleName                       = mintCosmos.ModuleName
+	StoreKey                         = mintCosmos.StoreKey
+	DefaultParamspace                = mintCosmos.DefaultParamspace
+)
+
+var(
+	NewCosmosAppModule    = mintCosmos.NewAppModule
+	NewKeeper                                  = mintCosmos.NewKeeper
+
+)
+
+type (
+	CosmosAppModule = mintCosmos.AppModule
+	CosmosAppModuleBasic = mintCosmos.AppModuleBasic
+	Keeper = mintCosmos.Keeper
+)
