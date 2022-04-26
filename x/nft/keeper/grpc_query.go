9f3a1d3787fb4c17c3a5b1e2699272db4c28ab0c
go-btsg <junkai.pro@gmail.com>
2022-04-26 13:00:37 +1000

    bootstrap codebase for grpc and msg server

--- x/nft/keeper/grpc_query.go
+++ x/nft/keeper/grpc_query.go
@@ -1,1 +1,50 @@
 package keeper
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/bitsongofficial/go-bitsong/x/nft/types"
+)
+
+var _ types.QueryServer = Keeper{}
+
+func (k Keeper) NFTInfo(c context.Context, req *types.QueryNFTInfoRequest) (*types.QueryNFTInfoResponse, error) {
+	if req == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+	_ = ctx
+
+	// TODO: implement!
+	return &types.QueryNFTInfoResponse{}, nil
+}
+
+func (k Keeper) Metadata(c context.Context, req *types.QueryMetadataRequest) (*types.QueryMetadataResponse, error) {
+	if req == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+	_ = ctx
+
+	// TODO: implement!
+	return &types.QueryMetadataResponse{}, nil
+}
+
+func (k Keeper) Collection(c context.Context, req *types.QueryCollectionRequest) (*types.QueryCollectionResponse, error) {
+	if req == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+	_ = ctx
+
+	// TODO: implement!
+	return &types.QueryCollectionResponse{}, nil
+}
--- x/nft/keeper/msg_server.go
+++ x/nft/keeper/msg_server.go
@@ -1,1 +1,64 @@
 package keeper
+
+import (
+	"context"
+
+	"github.com/bitsongofficial/go-bitsong/x/nft/types"
+)
+
+type msgServer struct {
+	Keeper
+}
+
+var _ types.MsgServer = msgServer{}
+
+// NewMsgServerImpl returns an implementation of the token MsgServer interface
+// for the provided Keeper.
+func NewMsgServerImpl(keeper Keeper) types.MsgServer {
+	return &msgServer{Keeper: keeper}
+}
+
+func (m msgServer) CreateNFT(goCtx context.Context, msg *types.MsgCreateNFT) (*types.MsgCreateNFTResponse, error) {
+	// TODO: implement!
+	return &types.MsgCreateNFTResponse{}, nil
+}
+
+func (m msgServer) TransferNFT(goCtx context.Context, msg *types.MsgTransferNFT) (*types.MsgTransferNFTResponse, error) {
+	// TODO: implement!
+	return &types.MsgTransferNFTResponse{}, nil
+}
+
+func (m msgServer) SignMetadata(goCtx context.Context, msg *types.MsgSignMetadata) (*types.MsgSignMetadataResponse, error) {
+	// TODO: implement!
+	return &types.MsgSignMetadataResponse{}, nil
+}
+
+func (m msgServer) UpdateMetadata(goCtx context.Context, msg *types.MsgUpdateMetadata) (*types.MsgUpdateMetadataResponse, error) {
+	// TODO: implement!
+	return &types.MsgUpdateMetadataResponse{}, nil
+}
+
+func (m msgServer) UpdateMetadataAuthority(goCtx context.Context, msg *types.MsgUpdateMetadataAuthority) (*types.MsgUpdateMetadataAuthorityResponse, error) {
+	// TODO: implement!
+	return &types.MsgUpdateMetadataAuthorityResponse{}, nil
+}
+
+func (m msgServer) CreateCollection(goCtx context.Context, msg *types.MsgCreateCollection) (*types.MsgCreateCollectionResponse, error) {
+	// TODO: implement!
+	return &types.MsgCreateCollectionResponse{}, nil
+}
+
+func (m msgServer) VerifyCollection(goCtx context.Context, msg *types.MsgVerifyCollection) (*types.MsgVerifyCollectionResponse, error) {
+	// TODO: implement!
+	return &types.MsgVerifyCollectionResponse{}, nil
+}
+
+func (m msgServer) UnverifyCollection(goCtx context.Context, msg *types.MsgUnverifyCollection) (*types.MsgUnverifyCollectionResponse, error) {
+	// TODO: implement!
+	return &types.MsgUnverifyCollectionResponse{}, nil
+}
+
+func (m msgServer) UpdateCollectionAuthority(goCtx context.Context, msg *types.MsgUpdateCollectionAuthority) (*types.MsgUpdateCollectionAuthorityResponse, error) {
+	// TODO: implement!
+	return &types.MsgUpdateCollectionAuthorityResponse{}, nil
+}
