75d2d5d4a5af158986e81daaef0cbf9c2bf0e8f3
go-btsg <junkai.pro@gmail.com>
2022-04-26 12:28:29 +1000

    initiate empty files for nft module

---
+++ x/nft/types/params.go
@@ -0,0 +1,69 @@
+package types
+
+import (
+	"fmt"
+
+	"github.com/bitsongofficial/go-bitsong/types"
+	"gopkg.in/yaml.v2"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+)
+
+var _ paramtypes.ParamSet = (*Params)(nil)
+
+// parameter keys
+var (
+	KeyIssuePrice = []byte("IssuePrice")
+)
+
+func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(KeyIssuePrice, &p.IssuePrice, validateIssuePrice),
+	}
+}
+
+// NewParams constructs a new Params instance
+func NewParams(issuePrice sdk.Coin) Params {
+	return Params{
+		IssuePrice: issuePrice,
+	}
+}
+
+// ParamKeyTable returns the TypeTable for the token module
+func ParamKeyTable() paramtypes.KeyTable {
+	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
+}
+
+// DefaultParams return the default params
+func DefaultParams() Params {
+	return Params{
+		IssuePrice: sdk.NewCoin(types.BondDenom, sdk.NewInt(1000000)),
+	}
+}
+
+// String returns a human readable string representation of the parameters.
+func (p Params) String() string {
+	out, _ := yaml.Marshal(p)
+	return string(out)
+}
+
+// ValidateParams validates the given params
+func ValidateParams(p Params) error {
+	if err := validateIssuePrice(p.IssuePrice); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateIssuePrice(i interface{}) error {
+	v, ok := i.(sdk.Coin)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	if v.IsNegative() {
+		return fmt.Errorf("issue price should not be negative")
+	}
+	return nil
+}
