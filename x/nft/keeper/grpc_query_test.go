3798be686a008d0fb02433306c022f5a9b75c003
go-btsg <junkai.pro@gmail.com>
2022-05-06 11:23:34 +1000

    in progress grpc query code

--- x/nft/keeper/grpc_query_test.go
+++ x/nft/keeper/grpc_query_test.go
@@ -1,6 +1,43 @@
 package keeper_test
 
+import (
+	"github.com/bitsongofficial/go-bitsong/x/nft/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 // TODO: test
-// NFTInfo
+// NFTsByOwner
 // Metadata
 // Collection
+
+func (suite *KeeperTestSuite) TestGRPCNFTInfo() {
+	// TODO: call a utility function to create an nft
+
+	tests := []struct {
+		testCase         string
+		nftId            uint64
+		expectPass       bool
+		expectedNFT      types.NFT
+		expectedMetadata types.Metadata
+	}{
+		{
+			"not existing nft id query",
+			0,
+			false,
+			types.NFT{},
+			types.Metadata{},
+		},
+		// TODO: add case for querying by existing nftId
+	}
+
+	for _, tc := range tests {
+		resp, err := suite.app.NFTKeeper.NFTInfo(sdk.WrapSDKContext(suite.ctx), &types.QueryNFTInfoRequest{})
+		if tc.expectPass {
+			suite.Require().NoError(err)
+			suite.Require().Equal(resp.Nft, tc.expectedNFT)
+			suite.Require().Equal(resp.Metadata, tc.expectedMetadata)
+		} else {
+			suite.Require().Error(err)
+		}
+	}
+}
