2c03aaf290e9ce8570328cf273f16ad7e1964e38
go-btsg <junkai.pro@gmail.com>
2022-06-06 11:47:59 +1000

    add test for BidsByAuction,BidsByBidder,BidderMetadata

--- x/marketplace/keeper/grpc_query_test.go
+++ x/marketplace/keeper/grpc_query_test.go
@@ -241,6 +241,120 @@ func (suite *KeeperTestSuite) TestGRPCAuction() {
 	}
 }
 
-// TODO: add test for BidsByAuction
-// TODO: add test for BidsByBidder
-// TODO: add test for BidderMetadata
+func (suite *KeeperTestSuite) TestGRPCBidsByAuction() {
+	bidder1 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	bidder2 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	now := time.Now().UTC()
+	suite.ctx = suite.ctx.WithBlockTime(now.Add(time.Second))
+
+	// get not available bid
+	_, err := suite.app.MarketplaceKeeper.GetBid(suite.ctx, 1, bidder1)
+	suite.Require().Error(err)
+
+	// get all bids when not available
+	allBids := suite.app.MarketplaceKeeper.GetAllBids(suite.ctx)
+	suite.Require().Len(allBids, 0)
+
+	bids := []types.Bid{
+		{
+			Bidder:    bidder1.String(),
+			AuctionId: 1,
+			Amount:    1000000,
+		},
+		{
+			Bidder:    bidder2.String(),
+			AuctionId: 1,
+			Amount:    1200000,
+		},
+		{
+			Bidder:    bidder1.String(),
+			AuctionId: 2,
+			Amount:    1000000,
+		},
+	}
+
+	for _, bid := range bids {
+		suite.app.MarketplaceKeeper.SetBid(suite.ctx, bid)
+	}
+
+	// test GetBidsAuction
+	resp, err := suite.app.MarketplaceKeeper.BidsByAuction(sdk.WrapSDKContext(suite.ctx), &types.QueryBidsByAuctionRequest{
+		Id: 1,
+	})
+	suite.Require().Len(resp.Bids, 2)
+}
+
+func (suite *KeeperTestSuite) TestGRPCBidsByBidder() {
+	bidder1 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	bidder2 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	now := time.Now().UTC()
+	suite.ctx = suite.ctx.WithBlockTime(now.Add(time.Second))
+
+	bids := []types.Bid{
+		{
+			Bidder:    bidder1.String(),
+			AuctionId: 1,
+			Amount:    1000000,
+		},
+		{
+			Bidder:    bidder2.String(),
+			AuctionId: 1,
+			Amount:    1200000,
+		},
+		{
+			Bidder:    bidder1.String(),
+			AuctionId: 2,
+			Amount:    1000000,
+		},
+	}
+
+	for _, bid := range bids {
+		suite.app.MarketplaceKeeper.SetBid(suite.ctx, bid)
+	}
+
+	// test GetBidsByBidder
+	resp, err := suite.app.MarketplaceKeeper.BidsByBidder(sdk.WrapSDKContext(suite.ctx), &types.QueryBidsByBidderRequest{
+		Bidder: bidder1.String(),
+	})
+	suite.Require().NoError(err)
+	suite.Require().Len(resp.Bids, 2)
+}
+
+func (suite *KeeperTestSuite) TestGRPCBidderMetadata() {
+
+	bidder1 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	bidder2 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	now := time.Now().UTC()
+	suite.ctx = suite.ctx.WithBlockTime(now.Add(time.Second))
+
+	bidderdata := []types.BidderMetadata{
+		{
+			Bidder:           bidder1.String(),
+			LastAuctionId:    1,
+			LastBid:          5000,
+			LastBidTimestamp: now.UTC(),
+			LastBidCancelled: false,
+		},
+		{
+			Bidder:           bidder2.String(),
+			LastAuctionId:    1,
+			LastBid:          10000,
+			LastBidTimestamp: now.UTC(),
+			LastBidCancelled: false,
+		},
+	}
+
+	for _, bm := range bidderdata {
+		suite.app.MarketplaceKeeper.SetBidderMetadata(suite.ctx, bm)
+	}
+
+	for _, bm := range bidderdata {
+		bidder, err := sdk.AccAddressFromBech32(bm.Bidder)
+		suite.Require().NoError(err)
+		b, err := suite.app.MarketplaceKeeper.BidderMetadata(sdk.WrapSDKContext(suite.ctx), &types.QueryBidderMetadataRequest{
+			Bidder: bidder.String(),
+		})
+		suite.Require().NoError(err)
+		suite.Require().Equal(b.BidderMetadata, bm)
+	}
+}
