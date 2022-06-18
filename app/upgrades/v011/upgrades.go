f167c655da34092b3ccc820d65e230ee40beddb6
angelorc <angelo@rcstart.it>
2022-06-18 12:58:34 +0200

    feat(fantoken): implementing upgrade handler

--- app/upgrades/v011/upgrades.go
+++ app/upgrades/v011/upgrades.go
@@ -3,7 +3,6 @@ package v011
 import (
 	appparams "github.com/bitsongofficial/go-bitsong/app/params"
 	fantokenkeeper "github.com/bitsongofficial/go-bitsong/x/fantoken/keeper"
-	fantokentypes "github.com/bitsongofficial/go-bitsong/x/fantoken/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
@@ -13,14 +12,19 @@ func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator,
 	ftk *fantokenkeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-		params := fantokentypes.DefaultParams()
+		newVM, err := mm.RunMigrations(ctx, configurator, vm)
+		if err != nil {
+			return newVM, err
+		}
+
+		ctx.Logger().Info("Updating fantoken fee denom")
+		params := ftk.GetParamSet(ctx)
 		params.IssueFee.Denom = appparams.DefaultBondDenom
 		params.MintFee.Denom = appparams.DefaultBondDenom
 		params.BurnFee.Denom = appparams.DefaultBondDenom
 		params.TransferFee.Denom = appparams.DefaultBondDenom
-
 		ftk.SetParamSet(ctx, params)
 
-		return mm.RunMigrations(ctx, configurator, vm)
+		return newVM, err
 	}
 }
