944d558374f6fa134d95f678d90e2094b2e12766
angelorc <angelo@rcstart.it>
2022-06-18 12:47:57 +0200

    feat(fantoken): implementing upgrade handler

--- app/upgrades/v011/upgrades.go
+++ app/upgrades/v011/upgrades.go
@@ -1,13 +1,26 @@
 package v011
 
 import (
+	appparams "github.com/bitsongofficial/go-bitsong/app/params"
+	fantokenkeeper "github.com/bitsongofficial/go-bitsong/x/fantoken/keeper"
+	fantokentypes "github.com/bitsongofficial/go-bitsong/x/fantoken/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator) upgradetypes.UpgradeHandler {
+func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator,
+	ftk *fantokenkeeper.Keeper,
+) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		params := fantokentypes.DefaultParams()
+		params.IssueFee.Denom = appparams.DefaultBondDenom
+		params.MintFee.Denom = appparams.DefaultBondDenom
+		params.BurnFee.Denom = appparams.DefaultBondDenom
+		params.TransferFee.Denom = appparams.DefaultBondDenom
+
+		ftk.SetParamSet(ctx, params)
+
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
