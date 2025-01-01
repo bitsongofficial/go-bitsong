13dd837ca6024d64114564ab306835da0aa7cada
hard-nett <hardnettt@proton.me>
2025-01-01 14:05:19 -0500

    wip: init-from-state

--- app/upgrades/v011/constants.go
+++ app/upgrades/v011/constants.go
@@ -3,7 +3,6 @@ package v011
 import (
 	"github.com/bitsongofficial/go-bitsong/app/upgrades"
 	fantokentypes "github.com/bitsongofficial/go-bitsong/x/fantoken/types"
-	merkledroptypes "github.com/bitsongofficial/go-bitsong/x/merkledrop/types"
 	store "github.com/cosmos/cosmos-sdk/store/types"
 )
 
@@ -15,6 +14,6 @@ var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateV11UpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
-		Added: []string{fantokentypes.ModuleName, merkledroptypes.ModuleName},
+		Added: []string{fantokentypes.ModuleName, "merkledrop"},
 	},
 }
