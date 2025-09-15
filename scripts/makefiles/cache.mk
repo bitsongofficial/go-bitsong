5afa3c201db298941dd9de60dc30f12aac6cf1a9
hard-nett <hardnettt@proton.me>
2025-09-15 11:54:24 -0400

    tests: wire in tools for sh ci workflows

---
+++ scripts/makefiles/cache.mk
@@ -0,0 +1,57 @@
+$(BTSG_DEVCACHE):
+	@echo "creating .cache dir structure..."
+	mkdir -p $@
+	mkdir -p $(BTSG_DEVCACHE_BIN)
+	mkdir -p $(BTSG_DEVCACHE_INCLUDE)
+	mkdir -p $(BTSG_DEVCACHE_VERSIONS)
+	mkdir -p $(BTSG_DEVCACHE_NODE_MODULES)
+	mkdir -p $(BTSG_DEVCACHE)/run
+cache: $(BTSG_DEVCACHE)
+
+# $(GIT_CHGLOG_VERSION_FILE): $(BTSG_DEVCACHE)
+# 	@echo "installing git-chglog $(GIT_CHGLOG_VERSION) ..."
+# 	rm -f $(GIT_CHGLOG)
+# 	GOBIN=$(BTSG_DEVCACHE_BIN) go install github.com/git-chglog/git-chglog/cmd/git-chglog@$(GIT_CHGLOG_VERSION)
+# 	rm -rf "$(dir $@)"
+# 	mkdir -p "$(dir $@)"
+# 	touch $@
+# $(GIT_CHGLOG): $(GIT_CHGLOG_VERSION_FILE)
+
+$(MOCKERY_VERSION_FILE): $(BTSG_DEVCACHE)
+	@echo "installing mockery $(MOCKERY_VERSION) ..."
+	rm -f $(MOCKERY)
+	GOBIN=$(BTSG_DEVCACHE_BIN) go install -ldflags '-s -w -X github.com/vektra/mockery/v2/pkg/config.SemVer=$(MOCKERY_VERSION)' github.com/vektra/mockery/v2@v$(MOCKERY_VERSION)
+	rm -rf "$(dir $@)"
+	mkdir -p "$(dir $@)"
+	touch $@
+$(MOCKERY): $(MOCKERY_VERSION_FILE)
+
+$(GOLANGCI_LINT_VERSION_FILE): $(BTSG_DEVCACHE)
+	@echo "installing golangci-lint $(GOLANGCI_LINT_VERSION) ..."
+	rm -f $(GOLANGCI_LINT)
+	GOBIN=$(BTSG_DEVCACHE_BIN) go install github.com/golangci/golangci-lint/cmd/golangci-lint@$(GOLANGCI_LINT_VERSION)
+	rm -rf "$(dir $@)"
+	mkdir -p "$(dir $@)"
+	touch $@
+$(GOLANGCI_LINT): $(GOLANGCI_LINT_VERSION_FILE)
+
+$(STATIK_VERSION_FILE): $(BTSG_DEVCACHE)
+	@echo "Installing statik $(STATIK_VERSION) ..."
+	rm -f $(STATIK)
+	GOBIN=$(BTSG_DEVCACHE_BIN) $(GO) install github.com/rakyll/statik@$(STATIK_VERSION)
+	rm -rf "$(dir $@)"
+	mkdir -p "$(dir $@)"
+	touch $@
+$(STATIK): $(STATIK_VERSION_FILE)
+
+$(COSMOVISOR_VERSION_FILE): $(BTSG_DEVCACHE)
+	@echo "installing cosmovisor $(COSMOVISOR_VERSION) ..."
+	rm -f $(COSMOVISOR)
+	GOBIN=$(BTSG_DEVCACHE_BIN) $(GO) install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@$(COSMOVISOR_VERSION)
+	rm -rf "$(dir $@)"
+	mkdir -p "$(dir $@)"
+	touch $@
+$(COSMOVISOR): $(COSMOVISOR_VERSION_FILE)
+
+cache-clean:
+	rm -rf $(BTSG_DEVCACHE)
